75613141d23a2bfd7e38cd00b4faa8f30bebed7b
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2022-02-07 17:10:16 +0000

    terraform code to deploy http asg ubuntu instance: - needs key and dns_zone - todo: cert and https infra

--- common/database.py
+++ common/database.py
@@ -4,7 +4,7 @@ import pymongo
 
 
 class Database:
-    URI = "mongodb://127.0.0.1:27017/fullstack"
+    URI = os.environ['DB_URL']
     DATABASE = pymongo.MongoClient(URI).get_default_database()
 
     @staticmethod
--- common/utils.py
+++ common/utils.py
@@ -5,7 +5,7 @@ import re
 class Utils:
     @staticmethod
     def email_is_valid(email: str) -> bool:
-        email_address_matcher = re.compile(r'^[\w-]+@([\w-]+\.)+[\w]+$')
+        email_address_matcher = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
         return True if email_address_matcher.match(email) else False
 
     @staticmethod
